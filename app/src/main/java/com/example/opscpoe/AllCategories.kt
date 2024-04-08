e9831236912bfc6f3cfe6d71b406beabc38c9f9d
lab_services_student <ST10029778@vcconnect.edu.za>
2024-04-08 20:55:05 +0200

    added code to the AllCategories

--- app/src/main/java/com/example/opscpoe/AllCategories.kt
+++ app/src/main/java/com/example/opscpoe/AllCategories.kt
@@ -2,10 +2,20 @@ package com.example.opscpoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class AllCategories : AppCompatActivity() {
+    private lateinit var backBtn: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_categories)
-    }
+
+        // Hooks
+        backBtn = findViewById(R.id.back_pressed)
+
+        backBtn.setOnClickListener {
+            super.onBackPressed()
+        }
+}
 }
